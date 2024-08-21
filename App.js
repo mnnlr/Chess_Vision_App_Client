84372d4b80b4750b8527c365fe50f0daa17c326e
Yash Maurya <yashmaurya2109@gmail.com>
2024-08-21 16:05:21 +0530

    Added Register and Login Screen

--- App.js
+++ App.js
@@ -4,15 +4,18 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import Home from './screens/Home';
 import Register from './screens/Register';
-
+import Inital from './screens/Inital';
+import Login from './screens/Login';
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Register" component={Register} />
+      <Stack.Navigator initialRouteName="Register">
+        <Stack.Screen name="Home" options={{ headerShown: false }} component={Home} />
+        <Stack.Screen name="Login"  options={{ headerShown: false }} component={Login} />
+        <Stack.Screen name="Register"  options={{ headerShown: false }} component={Register} />
+        <Stack.Screen name="Initial"  options={{ headerShown: false }} component={Inital} />
       </Stack.Navigator>
       <StatusBar style="auto" />
     </NavigationContainer>
