43944b4ddfd1838cc87fcca049e704ea7fcb473f
Yash Maurya <yashmaurya2109@gmail.com>
2024-08-22 14:22:32 +0530

    Added Dark and Light theame

--- App.js
+++ App.js
@@ -7,21 +7,36 @@ import Register from './screens/Register';
 import SettingsScreen from './screens/SettingsScreen';
 import Inital from './screens/Inital';
 import Login from './screens/Login';
+import Theame from './componenet/Theame';
+import { Provider, useSelector } from 'react-redux';
+import { store } from './redux/Store';
 
 const Stack = createStackNavigator();
 
+function AppNavigator() {
+  const isDarkTheme = useSelector((state) => state.theme.isDarkTheme);
+
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Register">
+          <Stack.Screen name="Home" options={{ headerShown: false }} component={Home} />
+          <Stack.Screen name="Login" options={{ headerShown: false }} component={Login} />
+          <Stack.Screen name="Register" options={{ headerShown: false }} component={Register} />
+          <Stack.Screen name="Initial" options={{ headerShown: false }} component={Inital} />
+          <Stack.Screen name='Setting' component={SettingsScreen} options={{ headerShown: false }} />
+          <Stack.Screen name='Theame' component={Theame} options={{ headerShown: false }} />
+        </Stack.Navigator>
+        <StatusBar style={isDarkTheme ? 'light' : 'dark'} />
+      </NavigationContainer>
+    </>
+  );
+}
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" options={{ headerShown: false }} component={Home} />
-        <Stack.Screen name="Login"  options={{ headerShown: false }} component={Login} />
-        <Stack.Screen name="Register"  options={{ headerShown: false }} component={Register} />
-        <Stack.Screen name="Initial"  options={{ headerShown: false }} component={Inital} />
-        <Stack.Screen name='Setting' component={SettingsScreen} options={{headerShown:false}}/>
-
-      </Stack.Navigator>
-      <StatusBar style="auto" />
-    </NavigationContainer>
+    <Provider store={store}>
+      <AppNavigator />
+    </Provider>
   );
 }
--- screens/Login.js
+++ screens/Login.js
@@ -1,7 +1,9 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React from 'react';
 import WaveInput from '../componenet/WaveInput';
+import { useNavigation } from '@react-navigation/native';
 const Login = () => {
+  const navigation = useNavigation();
     return (
         <View style={styles.container}>
           <View style={styles.gradientContainer}>
@@ -16,7 +18,7 @@ const Login = () => {
               <WaveInput label="Email" />
               <WaveInput label="Password" />
             </View>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.button}>
               <Text style={styles.buttonText}>Sign in</Text>
             </TouchableOpacity>
           </View>
